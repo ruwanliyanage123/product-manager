f4869c57d57deb6438bdf62a69532f610586cd51
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-09-29 09:14:14 +0530

    Fix a database connection bug

--- src/main/java/com/codejava/AppController.java
+++ src/main/java/com/codejava/AppController.java
@@ -1,8 +1,12 @@
 package com.codejava;
 
+import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -10,7 +14,9 @@ public class AppController {
     private ProductService productService;
 
     @RequestMapping("/")
-    public String viewHomePage(){
+    public String viewHomePage(Model model){
+        List<Product> productList = productService.listAll();
+        model.addAttribute("productList",productList);
         return "index";
     }
 }
